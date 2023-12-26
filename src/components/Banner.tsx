7e2a36ceaf05e0267cfdce6ff43cb91c311a9283
Aykut <aykutnazifov@gmail.com>
2023-12-26 19:19:49 +0200

    initial commit

---
+++ src/components/Banner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div className='bg-red-500 text-white h-12 flex items-center justify-center px-4 text-center text-sm md:text-base'>Free delivery for all orders over $50.</div>
+    )
+}
+
+export default Banner
