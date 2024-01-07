ea70bac8a146c78c7a641bd0499ad86bcd6aebc2
Aykut <aykutnazifov@gmail.com>
2024-01-07 21:04:12 +0200

    main pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import FeaturedItems from "@/components/FeaturedItems";
+import Offer from "@/components/Offer";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
-    <div>order</div>
+    <div>
+      <Slider />
+      <FeaturedItems />
+      <Offer />
+    </div>
   )
 }
